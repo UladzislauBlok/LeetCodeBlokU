cf53e3c5072b003a7615fa3d59e5aa6ff219b010
Uladzislau Blok <blokv75@gmail.com>
2025-03-08 14:42:37 +0100

    Remove Duplicates from Sorted List II

---
+++ src/main/java/org/ubdev/task/list/medium/RemoveDuplicatesSortedListII.java
@@ -0,0 +1,27 @@
+package org.ubdev.task.list.medium;
+
+import org.ubdev.domain.ListNode;
+
+public class RemoveDuplicatesSortedListII {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        ListNode pointer = dummyHead;
+        int lastVal = Integer.MIN_VALUE;
+        while (head.next != null) {
+            if (head.val != lastVal && head.val != head.next.val) {
+                pointer.next = new ListNode(head.val);
+                pointer = pointer.next;
+            }
+            lastVal = head.val;
+            head = head.next;
+        }
+        if (head.val != lastVal) {
+            pointer.next = new ListNode(head.val);
+        }
+        return dummyHead.next;
+    }
+}
