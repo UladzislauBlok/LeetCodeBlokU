b8431555233f0e5f63f86bfcaf21e6e75bf28390
Uladzislau Blok <blokv75@gmail.com>
2025-03-11 21:28:22 +0100

    Ransom Note

---
+++ src/test/java/org/ubdev/task/hashmap/easy/RansomNoteTest.java
@@ -0,0 +1,40 @@
+package org.ubdev.task.hashmap.easy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RansomNoteTest {
+
+    private RansomNote solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new RansomNote();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void isIsomorphicReturnsExpectedResult(String ransomNote, String magazine, boolean expected) {
+        // given
+
+        // when
+        boolean actual = solution.canConstruct(ransomNote, magazine);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> isIsomorphicReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of("a", "b", false),
+                Arguments.of("aa", "ab", false),
+                Arguments.of("aa", "aab", true)
+        );
+    }
+}
