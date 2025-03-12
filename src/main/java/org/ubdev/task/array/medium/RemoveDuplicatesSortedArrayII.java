468a2f5ce9bb22f5bf61bf21cc9d17bfb20c55bb
Uladzislau Blok <blokv75@gmail.com>
2025-03-12 19:01:28 +0100

    Remove Duplicates from Sorted Array II

---
+++ src/main/java/org/ubdev/task/array/medium/RemoveDuplicatesSortedArrayII.java
@@ -0,0 +1,23 @@
+package org.ubdev.task.array.medium;
+
+public class RemoveDuplicatesSortedArrayII {
+
+    public int removeDuplicates(int[] nums) {
+        int left  = 1;
+        int right = 1;
+        int currNumCounter = 1;
+        while (right < nums.length) {
+            if (nums[right] != nums[right - 1]) {
+                nums[left++] = nums[right];
+                currNumCounter = 1;
+            } else {
+                currNumCounter++;
+                if (currNumCounter <= 2) {
+                    nums[left++] = nums[right];
+                }
+            }
+            right++;
+        }
+        return left;
+    }
+}
---
+++ src/test/java/org/ubdev/task/array/medium/RemoveDuplicatesSortedArrayIITest.java
@@ -0,0 +1,42 @@
+package org.ubdev.task.array.medium;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RemoveDuplicatesSortedArrayIITest {
+
+    private RemoveDuplicatesSortedArrayII solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new RemoveDuplicatesSortedArrayII();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void pivotArrayReturnsExpectedResult(int[] nums, int expectedK, int[] expectedArray) {
+        // given
+
+        // when
+        int actual = solution.removeDuplicates(nums);
+
+        // then
+        assertThat(actual).isEqualTo(expectedK);
+        for (int i = 0; i < expectedK; i++) {
+            assertThat(nums[i]).isEqualTo(expectedArray[i]);
+        }
+    }
+
+    static Stream<Arguments> pivotArrayReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new int[]{1,1,1,2,2,3}, 5, new int[]{1,1,2,2,3,-1}),
+                Arguments.of(new int[]{0,0,1,1,1,1,2,3,3}, 7, new int[]{0,0,1,1,2,3,3,-1,-1})
+        );
+    }
+}
