66e754bc046a16537657cbce498da3f833e47029
Uladzislau Blok <blokv75@gmail.com>
2025-03-15 20:20:13 +0100

    PowXN

---
+++ src/main/java/org/ubdev/task/math/medium/PowXN.java
@@ -0,0 +1,20 @@
+package org.ubdev.task.math.medium;
+
+public class PowXN {
+
+    public double myPow(double x, int n) {
+        double result = 1;
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                result *= x;
+            }
+            x *= x;
+            n >>= 1;
+        }
+        return result;
+    }
+}
