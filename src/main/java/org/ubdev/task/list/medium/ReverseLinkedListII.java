6d290d654c792df1c1e4063a965474cd385adef8
Uladzislau Blok <blokv75@gmail.com>
2025-03-15 21:29:49 +0100

    Reverse Linked List II

---
+++ src/main/java/org/ubdev/task/list/medium/ReverseLinkedListII.java
@@ -0,0 +1,30 @@
+package org.ubdev.task.list.medium;
+
+import org.ubdev.domain.ListNode;
+
+public class ReverseLinkedListII {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right)
+            return head;
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+        ListNode curr = prev.next;
+        ListNode temp;
+        ListNode leftNode = curr;
+        ListNode leftPrevNode = prev;
+        for (int i = left; i <= right; i++) {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        leftNode.next = curr;
+        leftPrevNode.next = prev;
+        return dummyHead.next;
+    }
+}
---
+++ src/test/java/org/ubdev/task/list/medium/ReverseLinkedListIITest.java
@@ -0,0 +1,44 @@
+package org.ubdev.task.list.medium;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.ubdev.domain.ListNode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.ubdev.util.ListUtil.buildListNode;
+
+class ReverseLinkedListIITest {
+
+    private ReverseLinkedListII solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new ReverseLinkedListII();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void rotateRightReturnsExpectedResult(ListNode head, int left, int right, ListNode expected) {
+        // given
+
+        // when
+        ListNode actual = solution.reverseBetween(head, left, right);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> rotateRightReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(buildListNode(List.of(1,2,3)), 1 , 3, buildListNode(List.of(3, 2, 1))),
+                Arguments.of(buildListNode(List.of(3,5)), 1 , 2, buildListNode(List.of(5,3))),
+                Arguments.of(buildListNode(List.of(1,2,3,4,5)), 2 , 4, buildListNode(List.of(1,4,3,2,5))),
+                Arguments.of(buildListNode(List.of(5)), 1, 1, buildListNode(List.of(5)))
+        );
+    }
+}
