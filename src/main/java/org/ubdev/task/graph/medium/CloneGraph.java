432be165020b52e80db9a4f8101caf167916e66d
Uladzislau Blok <blokv75@gmail.com>
2025-04-01 23:09:58 +0200

    Clone Graph

---
+++ src/main/java/org/ubdev/task/graph/medium/CloneGraph.java
@@ -0,0 +1,24 @@
+package org.ubdev.task.graph.medium;
+
+import org.ubdev.domain.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CloneGraph {
+
+    private final Map<Integer, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+        if (map.containsKey(node.val))
+            return map.get(node.val);
+        Node copy = new Node(node.val);
+        map.put(node.val, copy);
+        for (Node neighbor : node.neighbors) {
+            copy.neighbors.add(cloneGraph(neighbor));
+        }
+        return copy;
+    }
+}
