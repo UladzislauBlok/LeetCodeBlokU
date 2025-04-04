cc29b8c234cdcb3f0c7ee77862de04f6f70e04a4
Uladzislau Blok <blokv75@gmail.com>
2025-04-04 23:21:38 +0200

    Snakes and Ladders

--- src/main/java/org/ubdev/task/graph/medium/SnakesLadders.java
+++ src/main/java/org/ubdev/task/graph/medium/SnakesLadders.java
@@ -1,20 +1,47 @@
 package org.ubdev.task.graph.medium;
 
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class SnakesLadders {
 
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        int[] distances = new int[n * 2];
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        int pointer = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(1);
+        boolean[] visited = new boolean[n * n + 1];
+        visited[1] = true;
+        int moves = 0;
+
+        while (!queue.isEmpty()) {
+            for (int i = queue.size(); i > 0; i--) {
+                int current = queue.poll();
+                if (current == n * n) {
+                    return moves;
+                }
+                for (int k = current + 1; k <= Math.min(current + 6, n * n); k++) {
+                    int boardVal = getFromBoard(n, k, board);
+                    int next = k;
+                    if (boardVal != -1) {
+                        next = boardVal;
+                    }
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        queue.offer(next);
+                    }
+                }
+            }
+            moves++;
+        }
+        return -1;
     }
 
-    private int getBoardPosition(int n, int pos) {
+    private int getFromBoard(int n, int pos, int[][] board) {
+        pos = pos - 1;
         int row = pos / n;
         boolean leftToRight = (row % 2 == 0);
         int posInRow = pos % n;
-        return leftToRight ? posInRow - 1 : n - posInRow;
+        int rowFromTheEnd = n - row - 1;
+        return leftToRight ? board[rowFromTheEnd][posInRow] : board[rowFromTheEnd][n - posInRow - 1];
     }
 }
