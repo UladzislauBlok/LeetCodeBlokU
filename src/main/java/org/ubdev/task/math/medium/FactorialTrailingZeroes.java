ef05fb5a26b488fc83566a4917021e77588f4b85
Uladzislau Blok <blokv75@gmail.com>
2025-04-21 17:44:51 +0200

    Factorial Trailing Zeroes

---
+++ src/main/java/org/ubdev/task/math/medium/FactorialTrailingZeroes.java
@@ -0,0 +1,13 @@
+package org.ubdev.task.math.medium;
+
+public class FactorialTrailingZeroes {
+
+    public int trailingZeroes(int n) {
+        int counter = 0;
+        while (n > 0) {
+            n = n / 5;
+            counter += n;
+        }
+        return counter;
+    }
+}
