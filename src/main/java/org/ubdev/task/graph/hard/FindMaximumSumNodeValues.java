a8d2edd05d2b769c9b2c6f831506a1889e8bb120
Uladzislau Blok <blokv75@gmail.com>
2025-05-24 18:11:18 +0200

    Find the Maximum Sum of Node Values

---
+++ src/main/java/org/ubdev/task/graph/hard/FindMaximumSumNodeValues.java
@@ -0,0 +1,17 @@
+package org.ubdev.task.graph.hard;
+
+public class FindMaximumSumNodeValues {
+
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long sum = 0;
+        long count = 0;
+        long minGain = Long.MAX_VALUE;
+        for (int n : nums) {
+            int x = n ^ k;
+            sum += Math.max(x, n);
+            if (x > n) count++;
+            minGain = Math.min(minGain, Math.abs(n - x));
+        }
+        return sum - (count % 2 != 0 ? minGain : 0);
+    }
+}
