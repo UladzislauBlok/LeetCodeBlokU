9ad838c22577285ad53d662a45d073b2bb9b8869
Uladzislau Blok <blokv75@gmail.com>
2025-05-25 23:47:19 +0200

    Super Ugly Number

---
+++ src/main/java/org/ubdev/task/array/medium/SuperUglyNumber.java
@@ -0,0 +1,26 @@
+package org.ubdev.task.array.medium;
+
+import java.util.PriorityQueue;
+
+public class SuperUglyNumber {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.offer(1);
+        int currentUglyNumber = 0;
+        while (n-- > 0) {
+            currentUglyNumber = minHeap.poll();
+            while (!minHeap.isEmpty() && minHeap.peek() == currentUglyNumber) { //remove duplicates
+                minHeap.poll();
+            }
+            for (int prime : primes) {
+                if (prime <= Integer.MAX_VALUE / currentUglyNumber) {
+                    minHeap.offer(prime * currentUglyNumber);
+                }
+                if (currentUglyNumber % prime == 0) {
+                    break;
+                }
+            }
+        }
+        return currentUglyNumber;
+    }
+}
