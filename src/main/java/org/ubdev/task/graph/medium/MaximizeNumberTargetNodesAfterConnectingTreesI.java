8bdcf79e0edc540e744ce0090d7359bbd6ea1c3f
Uladzislau Blok <blokv75@gmail.com>
2025-05-29 21:11:09 +0200

    Maximize the Number of Target Nodes After Connecting Trees I

--- src/main/java/org/ubdev/task/graph/medium/MaximizeNumberTargetNodesAfterConnectingTreesI.java
+++ src/main/java/org/ubdev/task/graph/medium/MaximizeNumberTargetNodesAfterConnectingTreesI.java
@@ -1,9 +1,14 @@
 package org.ubdev.task.graph.medium;
 
-import org.ubdev.domain.Node;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class MaximizeNumberTargetNodesAfterConnectingTreesI {
-	
+class MaximizeNumberTargetNodesAfterConnectingTreesI {
+
+    /**
+     * Build two adjacent list. Find best connect point in tree2. Get dfs from tree1 + bast result from tree 2
+     */
 	public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
         var g2 = build(edges2);
         int m = edges2.length + 1;
@@ -21,6 +26,9 @@ public class MaximizeNumberTargetNodesAfterConnectingTreesI {
         return ans;
     }
 
+    /**
+     * Build an adjacent list
+     */
     private List<Integer>[] build(int[][] edges) {
         int n = edges.length + 1;
         List<Integer>[] g = new List[n];
@@ -33,6 +41,13 @@ public class MaximizeNumberTargetNodesAfterConnectingTreesI {
         return g;
     }
 
+    /**
+     * @param g - adjacent list
+     * @param a - current vertex
+     * @param fa - previous vertex
+     * @param d - path length
+     * @return Max elements within the d
+     */
     private int dfs(List<Integer>[] g, int a, int fa, int d) {
         if (d < 0) {
             return 0;
