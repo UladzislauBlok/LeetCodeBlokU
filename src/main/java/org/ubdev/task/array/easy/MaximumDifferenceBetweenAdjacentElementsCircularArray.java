3636726a607732b5894f3f8ab23e2b6187207e9e
Uladzislau Blok <blokv75@gmail.com>
2025-06-12 20:19:36 +0200

    Maximum Difference Between Adjacent Elements in a Circular Array

---
+++ src/main/java/org/ubdev/task/array/easy/MaximumDifferenceBetweenAdjacentElementsCircularArray.java
@@ -0,0 +1,13 @@
+package org.ubdev.task.array.easy;
+
+public class MaximumDifferenceBetweenAdjacentElementsCircularArray {
+
+    public int maxAdjacentDistance(int[] nums) {
+        int max = Math.abs(nums[0] - nums[nums.length - 1]);
+        for (int i = 1; i < nums.length; i++) {
+            int diff = Math.abs(nums[i] - nums[i - 1]);
+            max = Math.max(max, diff);
+        }
+        return max;
+    }
+}
---
+++ src/test/java/org/ubdev/task/array/easy/MaximumDifferenceBetweenAdjacentElementsCircularArrayTest.java
@@ -0,0 +1,39 @@
+package org.ubdev.task.array.easy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaximumDifferenceBetweenAdjacentElementsCircularArrayTest {
+
+    private MaximumDifferenceBetweenAdjacentElementsCircularArray solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new MaximumDifferenceBetweenAdjacentElementsCircularArray();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int[] nums, int expected) {
+        // given
+
+        // when
+        int actual = solution.maxAdjacentDistance(nums);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,4}, 3),
+                Arguments.of(new int[]{-5,-10,-5}, 5)
+        );
+    }
+}
