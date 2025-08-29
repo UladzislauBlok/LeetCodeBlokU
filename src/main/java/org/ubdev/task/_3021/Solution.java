3384f06a030625265a7bf1655fb2b3de98e33a5d
Uladzislau Blok <blokv75@gmail.com>
2025-08-29 16:44:00 +0200

    Alice and Bob Playing Flower Game

---
+++ src/main/java/org/ubdev/task/_3021/Solution.java
@@ -0,0 +1,14 @@
+package org.ubdev.task._3021;
+
+import org.ubdev.util.LeetCodeName;
+import org.ubdev.util.Topic;
+import org.ubdev.util.Topics;
+
+@LeetCodeName("Alice and Bob Playing Flower Game")
+@Topics({Topic.MATH})
+class Solution {
+
+    public long flowerGame(int n, int m) {
+        return ((long) n * m) / 2;
+    }
+}
