0a38b71c60bbcb11a136882ee9db926d7ef67d03
Uladzislau Blok <blokv75@gmail.com>
2025-09-12 23:52:31 +0200

    Vowels Game in a String: rust

--- src/main/java/org/ubdev/task/_3227/solution.rs
+++ src/main/java/org/ubdev/task/_3227/solution.rs
@@ -1,1 +1,19 @@
+impl Solution {
+    pub fn does_alice_win_my(s: String) -> bool {
+        let res: String = s.chars().filter(|&x| "aeiou".contains(x)).collect();
+        if res.len() > 0 {
+            true
+        } else {
+            false
+        }
+    }
 
+    pub fn does_alice_win(s: String) -> bool {
+        let res: String = s.chars().filter(|&x| "aeiou".contains(x)).collect();
+        if res.len() > 0 {
+            true
+        } else {
+            false
+        }
+    }
+}
