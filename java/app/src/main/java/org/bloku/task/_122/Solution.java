7653590e172b32faf94484be7e918de019a0e7d5
Uladzislau Blok <blokv75@gmail.com>
2025-09-27 20:39:29 +0200

    Move more sols from legacy

---
+++ java/app/src/main/java/org/bloku/task/_122/Solution.java
@@ -0,0 +1,23 @@
+package org.bloku.task._122;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Best Time to Buy and Sell Stock II")
+@Topics({ARRAY, DYNAMIC_PROGRAMMING, GREEDY})
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
---
+++ java/app/src/main/java/org/bloku/task/_3208/Solution.java
@@ -0,0 +1,32 @@
+package org.bloku.task._3208;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Alternating Groups II")
+@Topics({ARRAY, SLIDING_WINDOW})
+class Solution {
+
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int[] fullCycle = new int[colors.length + k - 1];
+        System.arraycopy(colors, 0, fullCycle, 0, colors.length);
+        System.arraycopy(colors, 0, fullCycle, colors.length, k - 1);
+        int right = 0;
+        int left = 0;
+        int count = 0;
+        while (right < fullCycle.length - 1) {
+            right++;
+            if (fullCycle[right] == fullCycle[right - 1]) {
+                left = right;
+                continue;
+            }
+            if (right - left == k - 1) {
+                count++;
+                left++;
+            }
+        }
+        return count;
+    }
+}
