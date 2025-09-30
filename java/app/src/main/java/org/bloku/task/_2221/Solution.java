56d52ae95cd5044dd4d18a7f2af8f8edb5dcf9a5
Uladzislau Blok <blokv75@gmail.com>
2025-09-30 15:00:35 +0200

    Find Triangular Sum of an Array

---
+++ java/app/src/main/java/org/bloku/task/_2221/Solution.java
@@ -0,0 +1,22 @@
+package org.bloku.task._2221;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Find Triangular Sum of an Array")
+@Topics({ARRAY, MATH, SIMULATION})
+class Solution {
+
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        while (n > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            n--;
+        }
+        return nums[0];
+    }
+}
