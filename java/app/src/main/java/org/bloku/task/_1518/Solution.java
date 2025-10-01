b59e5ba8f0810e21529357191cd0605f2d99988e
Uladzislau Blok <blokv75@gmail.com>
2025-10-01 10:44:02 +0200

    Water Bottles

---
+++ java/app/src/main/java/org/bloku/task/_1518/Solution.java
@@ -0,0 +1,23 @@
+package org.bloku.task._1518;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Water Bottles")
+@Topics({MATH, SIMULATION})
+class Solution {
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count = 0;
+        int rest = 0;
+        while (numBottles > 0) {
+            count += numBottles;
+            int curr = numBottles + rest;
+            numBottles = curr / numExchange;
+            rest = curr % numExchange;
+        }
+        return count;
+    }
+}
