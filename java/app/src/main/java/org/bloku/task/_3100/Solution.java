ac57bbee4bc8bc2c836b4e011daa668cb731046a
Uladzislau Blok <blokv75@gmail.com>
2025-10-02 17:54:32 +0200

    Water Bottles II

---
+++ java/app/src/main/java/org/bloku/task/_3100/Solution.java
@@ -0,0 +1,22 @@
+package org.bloku.task._3100;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Water Bottles II")
+@Topics({MATH, SIMULATION})
+class Solution {
+
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int sum = numBottles, bottle = numBottles;
+        while (bottle >= numExchange) {
+            bottle -= numExchange;
+            numExchange++;
+            sum++;
+            bottle++;
+        }
+        return sum;
+    }
+}
