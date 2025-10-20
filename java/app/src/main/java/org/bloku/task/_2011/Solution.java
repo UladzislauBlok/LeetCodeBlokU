c787e7dfadad6d34428a4f60b80a39e7fc103b13
Uladzislau Blok <blokv75@gmail.com>
2025-10-20 08:44:37 +0200

    Final Value of Variable After Performing Operations

---
+++ java/app/src/main/java/org/bloku/task/_2011/Solution.java
@@ -0,0 +1,20 @@
+package org.bloku.task._2011;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Final Value of Variable After Performing Operations")
+@Topics({ARRAY, STRING, SIMULATION})
+class Solution {
+
+    public int finalValueAfterOperations(String[] operations) {
+        int res = 0;
+        for (String operation : operations) {
+            if (operation.contains("+")) res++;
+            else res--;
+        }
+        return res;
+    }
+}
