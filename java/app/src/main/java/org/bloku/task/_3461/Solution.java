b4268cdecfe82dd11119143043a29ea1a6958093
Uladzislau Blok <blokv75@gmail.com>
2025-10-23 11:29:12 +0200

    Check If Digits Are Equal in String After Operations I

---
+++ java/app/src/main/java/org/bloku/task/_3461/Solution.java
@@ -0,0 +1,24 @@
+package org.bloku.task._3461;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Check If Digits Are Equal in String After Operations I")
+@Topics({STRING, SIMULATION})
+class Solution {
+
+    public boolean hasSameDigits(String s) {
+        char[] chars = s.toCharArray();
+        int n = s.length();
+        while (n > 2) {
+            for (int i = 0; i < n - 1; i++) {
+                chars[i] = (char) (((chars[i] + chars[i + 1] - 2 * '0') % 10) + '0');
+            }
+            // System.out.println(Arrays.toString(chars));
+            n--;
+        }
+        return chars[0] == chars[1];
+    }
+}
