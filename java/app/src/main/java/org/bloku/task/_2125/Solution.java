f7a62149d2ed1c14d47de62adcfcd1596c2174bc
Uladzislau Blok <blokv75@gmail.com>
2025-10-27 13:21:10 +0100

    Number of Laser Beams in a Bank

---
+++ java/app/src/main/java/org/bloku/task/_2125/Solution.java
@@ -0,0 +1,24 @@
+package org.bloku.task._2125;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Number of Laser Beams in a Bank")
+@Topics({STRING, MATRIX})
+class Solution {
+
+    public int numberOfBeams(String[] bank) {
+        int n = bank.length, m = bank[0].length();
+        int count = 0;
+        int prev = 0;
+        for (int i = 0; i < n; i++) {
+            int curr = 0;
+            for (int j = 0; j < m; j++) curr += (bank[i].charAt(j) - '0');
+            count += prev * curr;
+            if (curr != 0) prev = curr;
+        }
+        return count;
+    }
+}
