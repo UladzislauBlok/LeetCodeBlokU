cefb1e0e5b149245f0f571e2a43629af2357ee05
Uladzislau Blok <blokv75@gmail.com>
2025-10-29 11:41:29 +0100

    Smallest Number With All Set Bits

---
+++ java/app/src/main/java/org/bloku/task/_3370/Solution.java
@@ -0,0 +1,20 @@
+package org.bloku.task._3370;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Smallest Number With All Set Bits")
+@Topics({BIT_MANIPULATION, MATH})
+class Solution {
+
+    public int smallestNumber(int n) {
+        int res = 1;
+        while (res < n) {
+            res = res << 1;
+            res++;
+        }
+        return res;
+    }
+}
