41f5177940e346e5096c107e3b967aaf148b669c
Uladzislau Blok <blokv75@gmail.com>
2025-10-30 13:06:39 +0100

    Minimum Number of Increments on Subarrays to Form a Target Array

---
+++ java/app/src/main/java/org/bloku/task/_1526/Solution.java
@@ -0,0 +1,23 @@
+package org.bloku.task._1526;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Minimum Number of Increments on Subarrays to Form a Target Array")
+@Topics({ARRAY, DYNAMIC_PROGRAMMING, GREEDY})
+class Solution {
+
+    // kind of find rectangle
+    // there are three cases: next elem > prev; next elem < prev; next elem == prev
+    // only first one will affect number of required operations
+    public int minNumberOperations(int[] target) {
+        int res = 0, prev = 0;
+        for (int num : target) {
+            if (prev < num) res += num - prev;
+            prev = num;
+        }
+        return res;
+    }
+}
