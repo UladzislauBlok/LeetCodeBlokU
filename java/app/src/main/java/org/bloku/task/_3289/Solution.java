f23a49a8e6fa49546043bc10a1b465d6f1750a6e
Uladzislau Blok <blokv75@gmail.com>
2025-10-31 10:44:08 +0100

    The Two Sneaky Numbers of Digitville

---
+++ java/app/src/main/java/org/bloku/task/_3289/Solution.java
@@ -0,0 +1,28 @@
+package org.bloku.task._3289;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("The Two Sneaky Numbers of Digitville")
+@Topics({ARRAY, HASH_TABLE})
+class Solution {
+
+    public int[] getSneakyNumbers(int[] nums) {
+        int n = nums.length;
+        int[] map = new int[n];
+        int[] res = new int[2];
+        res[0] = -1;
+        for (int num : nums) {
+            if (map[num]++ == 1) {
+                if (res[0] == -1) {
+                    res[0] = num;
+                } else {
+                    res[1] = num;
+                }
+            }
+        }
+        return res;
+    }
+}
