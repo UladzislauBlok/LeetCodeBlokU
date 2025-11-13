c56861a533af2897326812d0e068a54e2cdecbca
Uladzislau Blok <blokv75@gmail.com>
2025-11-13 15:08:07 +0100

    Maximum Number of Operations to Move Ones to the End

---
+++ java/app/src/main/java/org/bloku/task/_3228/Solution.java
@@ -0,0 +1,32 @@
+package org.bloku.task._3228;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Maximum Number of Operations to Move Ones to the End")
+@Topics({STRING, GREEDY, COUNTING})
+class Solution {
+
+    // looks simple. start on left most group of ones
+    // move it one by one to the right most (or right boundary)
+    public int maxOperations(String s) {
+        int count = 0;
+        int ones = 0;
+        boolean zero = false;
+        for (char c : s.toCharArray()) {
+            if (c == '0') {
+                zero = true;
+            } else {
+                if (zero) {
+                    count += ones;
+                    zero = false;
+                }
+                ones++;
+            }
+        }
+        if (zero) count += ones;
+        return count;
+    }
+}
