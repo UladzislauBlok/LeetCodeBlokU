19d380fb0c22ec2a32ba0f45b7f991f75311a6a1
Uladzislau Blok <blokv75@gmail.com>
2025-11-17 09:44:40 +0100

    Check If All 1's Are at Least Length K Places Away

---
+++ java/app/src/main/java/org/bloku/task/_1437/Solution.java
@@ -0,0 +1,26 @@
+package org.bloku.task._1437;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Check If All 1's Are at Least Length K Places Away")
+@Topics({ARRAY})
+class Solution {
+
+    public boolean kLengthApart(int[] nums, int k) {
+        int count = 0;
+        boolean skip = true;
+        for (int num : nums) {
+            if (num == 0) {
+                count++;
+            } else {
+                if (count < k && !skip) return false;
+                count = 0;
+                skip = false;
+            }
+        }
+        return true;
+    }
+}
