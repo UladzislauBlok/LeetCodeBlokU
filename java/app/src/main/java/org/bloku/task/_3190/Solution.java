dfee54b416a65614251e26ff0ca41c455e160117
Uladzislau Blok <blokv75@gmail.com>
2025-11-22 13:12:59 +0100

    Find Minimum Operations to Make All Elements Divisible by Three

---
+++ java/app/src/main/java/org/bloku/task/_3190/Solution.java
@@ -0,0 +1,18 @@
+package org.bloku.task._3190;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Find Minimum Operations to Make All Elements Divisible by Three")
+@Topics({ARRAY, MATH})
+class Solution {
+
+    // this is about find closest num that is divisible by 3
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        for (int num : nums) count += num % 3 == 0 ? 0 : 1;
+        return count;
+    }
+}
